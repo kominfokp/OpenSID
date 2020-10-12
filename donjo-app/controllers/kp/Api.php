2dc0297c4a63043395a6a2a5711465803fff341f
Diskominfo Kulon Progo <kp.gitlab@gmail.com>
2020-10-12 09:58:35 +0700

    add api config desa, update api kunjungan

--- donjo-app/controllers/kp/Api.php
+++ donjo-app/controllers/kp/Api.php
@@ -4,9 +4,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Api extends CI_Controller {
 
 	public function kunjungan() {
-		$kunjungan_hari_ini = $this->db->query("SELECT Jumlah AS Visitor FROM sys_traffic WHERE Tanggal=DATE(NOW()) LIMIT 1")->row_array();
+		$kunjungan_hari_ini = $this->db->query("SELECT Jumlah AS Visitor FROM sys_traffic WHERE Tanggal=DATE(NOW()) LIMIT 1")->row()->Visitor;
 
-		echo json_encode($kunjungan_hari_ini);
+		echo json_encode(['visitor'=>intval($kunjungan_hari_ini)]);
+		exit;
+	}
+
+	public function config_desa() {
+		$desa = $this->db->query("SELECT * FROM config LIMIT 1")->row();
+
+		echo json_encode(['desa'=>$desa]);
 		exit;
 	}
 
